1691506ff9e05a131ec876e989c73ac31786330d
dorianvh <dorhasenbroek@gmail.com>
2025-06-10 11:41:15 +0200

    Added load forecast to forecasting dataset generation

--- forecasting/load_forecast_data.py
+++ forecasting/load_forecast_data.py
@@ -10,7 +10,7 @@ forecast_type = 'day-ahead'
 df = pd.read_csv('../data/ID1_prices_germany.csv')
 # filter on start and end date
 # rename columns to match the expected format
-df = df.rename(columns={'Date': 'timestamp', 'ID1_price': 'ID1_price_eur_per_mwh)'})
+df = df.rename(columns={'Date': 'timestamp', 'ID1_price': 'ID1_price_eur_per_mwh'})
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 df = df[(df['timestamp'] >= start_date) & (df['timestamp'] <= end_date)]
 
@@ -20,6 +20,7 @@ df = df[(df['timestamp'] >= start_date) & (df['timestamp'] <= end_date)]
 solar_forecast = get_forecast_energy(production_type='solar', country=country, forecast_type=forecast_type, start_date=start_date, end_date=end_date, save_to_csv=False)
 wind_onshore_forecast = get_forecast_energy(production_type='wind_onshore', country=country, forecast_type=forecast_type, start_date=start_date, end_date=end_date, save_to_csv=False)
 wind_offshore_forecast = get_forecast_energy(production_type='wind_offshore', country=country, forecast_type=forecast_type, start_date=start_date, end_date=end_date, save_to_csv=False)
+load_forecast = get_forecast_energy(production_type='load', country=country, forecast_type=forecast_type, start_date=start_date, end_date=end_date, save_to_csv=False)
 
 #combine quarter hourly data into hourly data
 def combine_quarter_hourly_to_hourly(df):
@@ -39,6 +40,7 @@ def combine_quarter_hourly_to_hourly(df):
 solar_forecast = combine_quarter_hourly_to_hourly(solar_forecast)
 wind_onshore_forecast = combine_quarter_hourly_to_hourly(wind_onshore_forecast)
 wind_offshore_forecast = combine_quarter_hourly_to_hourly(wind_offshore_forecast)
+load_forecast = combine_quarter_hourly_to_hourly(load_forecast)
 
 da_prices = get_day_ahead_prices(bidding_zone='DE-LU', start_date=start_date, end_date=end_date, save_to_csv=False)
 
@@ -47,6 +49,7 @@ df = df[['timestamp', 'ID1_price_eur_per_mwh)']]  # Keep only relevant columns
 df = df.merge(solar_forecast, on='timestamp', how='left')
 df = df.merge(wind_onshore_forecast, on='timestamp', how='left')
 df = df.merge(wind_offshore_forecast, on='timestamp', how='left')
+df = df.merge(load_forecast, on='timestamp', how='left')
 df = df.merge(da_prices, on='timestamp', how='left')
 
 # Save the combined DataFrame to a CSV file
